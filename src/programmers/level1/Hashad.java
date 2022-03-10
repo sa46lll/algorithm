5f0969add62776bfd0e8a05c5d7ddea3f95207cd
sa46lll <9812smh@naver.com>
2022-03-10 23:50:36 +0900

    feat: Create Hashad.java

---
+++ src/programmers/level1/Hashad.java
@@ -0,0 +1,28 @@
+package programmers.level1;
+
+import java.io.IOException;
+public class Hashad {
+    public static void main(String[] args) throws IOException {
+        Solution sol = new Solution();
+        boolean result = sol.solution(10);
+        System.out.println(result);
+    }
+}
+
+// 하샤드 수
+class Solution {
+    public boolean solution (int x) throws IOException {
+        int sum = 0;
+        int a = x;
+
+        while (a >= 1) {
+            sum += a % 10;
+            a /= 10;
+        }
+
+        if (x % sum == 0) {
+            return true;
+        } else
+            return false;
+    }
+}
