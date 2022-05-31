4a5e98c28bf5ad8e2b631653cb9993436e006703
sa46lll <9812smh@naver.com>
2022-06-01 02:38:53 +0900

    feat: Array.java

---
+++ src/programmers/lesson/List/Array.java
@@ -0,0 +1,17 @@
+package programmers.lesson.List;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = new int[5];  // 0으로 초기화
+        String[] strs = new String[5];  // null 초기화
+        boolean[] bools = new boolean[5];  // false 초기화
+
+        arr[0] = 1;
+        arr[arr.length-1] = 5;
+
+        System.out.println(Arrays.toString(bools));
+        System.out.println(Arrays.toString(arr));
+    }
+}
