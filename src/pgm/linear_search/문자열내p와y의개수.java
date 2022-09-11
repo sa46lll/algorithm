ab1b97056e6a2b9f9a1cfaaee86825e705273a46
sa46lll <9812smh@naver.com>
2022-09-11 10:12:53 +0000

    feat: linear_search-2

---
+++ src/pgm/linear_search/문자열내p와y의개수.java
@@ -0,0 +1,14 @@
+package pgm.linear_search;
+
+public class 문자열내p와y의개수 {
+    static boolean solution(String s) {
+        int p = s.replaceAll("[^pP]", "").length();
+        int y = s.replaceAll("[^yY]", "").length();
+        return p ==y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("pPoooyY"));
+        System.out.println(solution("Pyy"));
+    }
+}
