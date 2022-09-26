b11d952b2df82a1d9162a13226f2a6e4c75e6046
sa46lll <9812smh@naver.com>
2022-09-26 12:19:53 +0000

    fix: Update linear_search-2

--- src/pgm/linear_search/문자열내p와y의개수.java
+++ src/pgm/linear_search/문자열내p와y의개수.java
@@ -2,9 +2,9 @@ package pgm.linear_search;
 
 public class 문자열내p와y의개수 {
     static boolean solution(String s) {
-        int p = s.replaceAll("[^pP]", "").length();
+        int p = s.replaceAll("[^pP]", "").length(); // p,P 제외
         int y = s.replaceAll("[^yY]", "").length();
-        return p ==y;
+        return p == y;
     }
 
     public static void main(String[] args) {
