22b4e3ebf5942d6e646e83593b5aaa04a0c23ceb
sa46lll <9812smh@naver.com>
2022-10-26 12:19:53 +0000

    feat: 티켓넘버
    recursive

---
+++ src/pgm/v2/티켓넘버.java
@@ -0,0 +1,23 @@
+package pgm.v2;
+
+public class 티켓넘버 {
+    static int solution(int[] numbers, int target) {
+        return sumCount(numbers, target, 0, 0);
+    }
+
+    static int sumCount(final int[] numbers, final int target, int i, int sum) {
+        if (i == numbers.length) {
+            if (sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+        return sumCount(numbers, target, i + 1, sum + numbers[i])
+                + sumCount(numbers, target, i + 1, sum - numbers[i]);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] {1, 1, 1, 1, 1}, 3));
+        System.out.println(solution(new int[] {4, 1, 2, 1}, 4));
+    }
+}
