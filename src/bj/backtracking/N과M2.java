0ea5f848148c7ef67815a545151622eb87881649
sa46lll <9812smh@naver.com>
2023-10-23 12:19:53 +0000

    feat: N과M2
    backtracking, dfs

--- src/bj/backtracking/N과M2.java
+++ src/bj/backtracking/N과M2.java
@@ -2,25 +2,24 @@ package bj.backtracking;
 
 import java.util.*;
 
-public class N과M {
+public class N과M2 {
 
     private int n;
     private int m;
-    private int[] uniqueNumbers; // 중복되지 않는 숫자들 모임 (방문한 숫자 모임)
-    private boolean[] visited; // 기존 숫자 방문 여부
-    private StringBuilder result = new StringBuilder();
+    private int[] uniqueNumbers;
+    private final StringBuilder result = new StringBuilder();
 
     public static void main(String[] args) {
-        new N과M().solution();
+        new N과M2().solution();
     }
 
     private void solution() {
         initializeFromUserInput();
-        generatePermutations(0);
+        generatePermutations(1, 0);
         System.out.println(result);
     }
 
-    private void generatePermutations(int depth) {
+    private void generatePermutations(int startIndex, int depth) {
         if (depth == m) {
             for (int i = 0; i < uniqueNumbers.length; i++) {
                 result.append(uniqueNumbers[i]).append(" ");
@@ -29,13 +28,9 @@ public class N과M {
             return;
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                uniqueNumbers[depth] = i;
-                generatePermutations(depth + 1);
-                visited[i] = false;
-            }
+        for (int i = startIndex; i <= n; i++) {
+            uniqueNumbers[depth] = i;
+            generatePermutations(i + 1, depth + 1);
         }
     }
 
@@ -44,6 +39,5 @@ public class N과M {
         n = scanner.nextInt();
         m = scanner.nextInt();
         uniqueNumbers = new int[m];
-        visited = new boolean[n + 1];
     }
 }
